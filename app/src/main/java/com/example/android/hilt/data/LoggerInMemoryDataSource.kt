857044260899817057dd77542a7bb6999d765604
jrg89 <jorgemartinalvarado@gmail.com>
2021-06-14 20:16:59 +0200

    Qualifiers. Two implementations for the same interface

--- app/src/main/java/com/example/android/hilt/data/LoggerInMemoryDataSource.kt
+++ app/src/main/java/com/example/android/hilt/data/LoggerInMemoryDataSource.kt
@@ -1,11 +1,10 @@
 package com.example.android.hilt.data
 
-import dagger.hilt.android.scopes.ActivityScoped
 import java.util.*
 import javax.inject.Inject
 
-@ActivityScoped
-class LoggerInMemoryDataSource @Inject constructor(): LoggerDataSource {
+
+class LoggerInMemoryDataSource @Inject constructor() : LoggerDataSource {
 
     private val logs = LinkedList<Log>()
 
--- app/src/main/java/com/example/android/hilt/data/LoggerLocalDataSource.kt
+++ app/src/main/java/com/example/android/hilt/data/LoggerLocalDataSource.kt
@@ -21,13 +21,11 @@ import android.os.Looper
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import javax.inject.Inject
-import javax.inject.Singleton
 
 /**
  * Data manager class that handles data manipulation between the database and the UI.
  */
-@Singleton
-class LoggerLocalDataSource @Inject constructor(private val logDao: LogDao) {
+class LoggerLocalDataSource @Inject constructor(private val logDao: LogDao) : LoggerDataSource {
 
     private val executorService: ExecutorService = Executors.newFixedThreadPool(4)
     private val mainThreadHandler by lazy {
